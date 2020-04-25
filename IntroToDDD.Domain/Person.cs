5995ec00bb95fd50d05c75d88ae9d433a066905e
Talesb <talesbatistafig@hotmail.com>
2020-04-25 17:54:19 -0300

    Criando primeira classe de dominio

---
+++ IntroToDDD.Domain/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntroToDDD.Domain
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
